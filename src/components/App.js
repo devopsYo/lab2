8147cdda7e4c9cea1e76eafab359bae1981011b9
Yoel <yoel_levy_1@hotmail.fr>
2022-01-13 19:12:12 +0200

    intit project lab2

---
+++ src/components/App.js
@@ -0,0 +1,44 @@
+import {Welcome, firstName} from './Welcome'
+
+let posts = [
+  {
+    id: '1',
+    title: 'post1'
+  },
+  {
+    id: '3',
+    title: 'post3'
+  },
+  {
+    id: '10',
+    title: 'post10'
+  }
+]
+
+
+const getUpperName = name => {
+  if (name) {
+      return name.toUpperCase()
+  }
+  return 'Foreign' 
+}
+
+const name = 'Yossef';
+
+function App() {
+  return (
+    <div>
+      {firstName == "Yossef" ? <Welcome coco={getUpperName(name)} dodo='by'/> : 'By'}
+      {posts.map((post, index) => 
+         <Welcome key={index} coco={post.id} dodo={index} />
+                )
+      }
+      {posts.map((post, index) =>          
+         getUpperName(name)
+                )
+      }
+    </div>
+  );
+}
+
+export default App;
